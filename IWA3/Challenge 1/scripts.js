edc3a383e2de0f37e1cc95f5efe16177ff661bc2
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-03-29 15:36:17 +0200

    Completed Challenge 1

---
+++ IWA3/Challenge 1/scripts.js
@@ -0,0 +1,8 @@
+
+
+//Corrected spelling of th word "form" to "from" and used correct syntax 
+import {company} from './configuration.js'
+import {year} from './configuration.js'
+
+const message = '© ' + company + year;
+document.querySelector('footer').innerText = message
