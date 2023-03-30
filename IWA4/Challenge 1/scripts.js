ee5aca72af8fbe8978fcb7283d46b3f3861044fc
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-03-30 15:26:43 +0200

    Added Comments

--- IWA4/Challenge 1/scripts.js
+++ IWA4/Challenge 1/scripts.js
@@ -1,15 +1,15 @@
-const date = 2050
-const status = "parent"
+const year = 2050 											//Deleted and used the correct syntax 
+const status = "parent" 									//Deleted and used the correct syntax 
 let count = 0
 
-if (date) {
+if (year) {													//Changed "date" to "year"
 	console.log('January', 'New Year`s Day')
 	console.log('March', 'Human Rights Day') 
-	console.log('April', 'Family Day') //logged "April and its holdiay"
-	console.log('April', 'Freedom Day') //logged "April and its holdiay"
-	count = count + 4
+	console.log('April', 'Family Day') 						//logged "April" and its holdiay
+	console.log('April', 'Freedom Day') 					//logged "April" and its holdiay
+	count = count + 4										//Removed the 'const'
 
-	if (status == "student") {
+	if (status == "student") {								//Removed the (=) and used the (==) operator for the boolean
 	  console.log('June', 'Youth Day')
 	  count = count + 1
   }
@@ -17,17 +17,17 @@ if (date) {
 	console.log('August', 'Women`s Day')
 	console.log('September', 'Heritage Day')
 	console.log('December', 'Day of Reconciliation')
-	count = count + 3
+	count = count + 3										//Removed the 'const'
 
-	if (status == "parent") {
+	if (status == "parent") {								//Removed the (=) and used the (==) operator for the boolean
 	  console.log('December', 'Christmas Day')
-	  count = count + 1
+	  count = count + 1	
   }
 
-	console.log('December', 'Day of Goodwill')
+	console.log('December', 'Day of Goodwill')				//logged the word "December" as a month
 	count = count + 1
 }
 
 console.log('Your status is:', status)
-console.log('The year is:', date)
+console.log('The year is:', year)							//used the declared word "year" and removed the word "date"
 console.log('The total holidays is:', count)
