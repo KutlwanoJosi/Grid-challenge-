b41d2d0c828901d645b5195994304edae6614a7b
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-04-03 16:31:09 +0200

    Fixed logic

--- IWA5/Challenge 1/scripts.js
+++ IWA5/Challenge 1/scripts.js
@@ -1,21 +1,23 @@
 const FREE_WARNING = 'Free shipping only applies to single customer orders'  //defined "FREE_WARNING" by adding const
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'  //defined "BANNED_WARNING" by adding const
 const NONE_SELECTED = '0'
-let location = 'RSA'  //defined location as "RSA"
-let shipping = 400 || 600 || 800 || null
-let currency = 'R' || '$'
-
+let location = 'RSA'  //declared location as "RSA"
+let shipping = 400 || 600 || 800 || null  // declared "shipping" and added 'or' statements 
+let currency = 'R'  //declared "currency" and added 'or' statements 
+const customers = '1'
 
 //fixed the structuring of the boolean
 if (location) { 
     shipping === 400 && currency === 'R' 
 
-    location = 'NAM'
+    location = 'NAM' //declared loaction as 'NAM'
 if (location){
+    currency = '$'
     shipping = 600 && currency === '$'  //added currency value 
 }
 
 else {
+    currency = '$'
     shipping = 800 && currency === '$'  //added currency value 
 }
 
@@ -27,21 +29,27 @@ let shirts = 150 * 'NONE_SELECTED'
 let batteries = 35 * 2  //added equal sign
 let pens = 5 * 'NONE_SELECTED' 
 
-shipping = null
-currency = $
-
 if (shoes + batteries + pens + shirts >= 1000 && currency === 'R' || 60 && currency === '$' ) {  //added the value of 60 and the currency
-	if (location = NAM && customers < 2) {
-			if (location = RSA)
-		    shipping = 0 || calcShipping
+	shipping = null  //declared shipping as 'null'
+    location = 'NAM'
+    if (location && customers < 2) {
+
+        location = 'RSA'  //declared location as "RSA"
+			if (location)
+		    shipping = null 
 		}
 	}
 
 if (shipping = 0 && customers !== 1) {
     console.log(FREE_WARNING) 
+
+    location = 'NK'  //declared location as 'NK'
+    if(location){
+        currency = null  //declared shipping as 'null'
+        console.log(BANNED_WARNING)
+}
 }
 
-location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+console.log('price', currency, shoes + batteries + pens + shirts + shipping)
 
-customers = '1'
-currency = null
+
