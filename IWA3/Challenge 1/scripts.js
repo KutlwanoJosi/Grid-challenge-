b91b87c67c844caab372ad1d80d5ea6c635778fa
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-04-03 15:36:21 +0200

    Debugged The Code

--- IWA3/Challenge 1/scripts.js
+++ IWA3/Challenge 1/scripts.js
@@ -1,8 +1,7 @@
 
 
 //Corrected spelling of th word "form" to "from" and used correct syntax 
-import {company} from './configuration.js'
-import {year} from './configuration.js'
+import {company, year} from './configuration.js'
 
 const message = '© ' + company + year;
 document.querySelector('footer').innerText = message
---
+++ IWA5/scripts.js
@@ -0,0 +1,47 @@
+const FREE_WARNING = 'Free shipping only applies to single customer orders'  //defined "FREE_WARNING" by adding const
+const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'  //defined "BANNED_WARNING" by adding const
+const NONE_SELECTED = '0'
+let location = 'RSA'  //defined location as "RSA"
+let shipping = 400 || 600 || 800 || null
+let currency = 'R' || '$'
+
+
+//fixed the structuring of the boolean
+if (location) { 
+    shipping === 400 && currency === 'R' 
+
+    location = 'NAM'
+if (location){
+    shipping = 600 && currency === '$'  //added currency value 
+}
+
+else {
+    shipping = 800 && currency === '$'  //added currency value 
+}
+
+}
+
+let shoes = 300 * 1
+let toys = 100 * 5  //added eqaul sign
+let shirts = 150 * 'NONE_SELECTED'
+let batteries = 35 * 2  //added equal sign
+let pens = 5 * 'NONE_SELECTED' 
+
+shipping = null
+currency = $
+
+if (shoes + batteries + pens + shirts >= 1000 && currency === 'R' || 60 && currency === '$' ) {  //added the value of 60 and the currency
+	if (location = NAM && customers < 2) {
+			if (location = RSA)
+		    shipping = 0 || calcShipping
+		}
+	}
+
+if (shipping = 0 && customers !== 1) {
+    console.log(FREE_WARNING) 
+}
+
+location = 'NK' ? console.log(WARNING) : console.log('price', currency, shoes + batteries + pens + shirts + shipping)
+
+customers = '1'
+currency = null
