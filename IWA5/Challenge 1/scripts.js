065a94e3e5bf1517f8fe3d559cee39b8e0f841dc
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-04-05 13:56:36 +0200

    Debugged My Code

--- IWA5/Challenge 1/scripts.js
+++ IWA5/Challenge 1/scripts.js
@@ -1,56 +1,43 @@
 const FREE_WARNING = 'Free shipping only applies to single customer orders'  //defined "FREE_WARNING" by adding const
 const BANNED_WARNING = 'Unfortunately we do not ship to your country of residence'  //defined "BANNED_WARNING" by adding const
 const NONE_SELECTED = null
-const customers = '1'
+const customers = 1
 let location = 'RSA'  //declared location as "RSA"
-let shipping = 400  // declared "shipping" 
-let currency = 'R'  //declared "currency" and added 'or' statements 
+let shipping = null  // declared "shipping" 
+let currency = null //declared "currency" and added 'or' statements 
 
-
-//fixed the structuring of the boolean
-if (location =='RSA') { 
-    currency == 'R'
+//fixed the structuring ofthe if statements 
+if (location === 'RSA') {
+    currency = 'R'
     shipping = 400
-
-        if (location == 'NAM'){  //declared loaction as 'NAM'
-            currency == '$'  //added currency value
-             const shipping = 600    
-    }
-
-        else {
-            currency == '$'  //added currency value
-            let shipping = 800    
-    }
-
-}
+  } else if (location === 'NAM') {
+    currency = '$'
+    shipping = 600
+  } else if (location === 'NK') { //declared location as 'NK'
+    shipping = null  //declared shipping as 'null'
+    currency = null
+    console.log(BANNED_WARNING);
+  } else {
+    currency = '$'
+    shipping = 800
+  }
 
 let shoes = 300 * 1
 let toys = 100 * 5  //added eqaul sign
 let shirts = 150 * NONE_SELECTED
 let batteries = 35 * 2  //added equal sign
-let pens = 5 * NONE_SELECTED 
+let pens = 5 * NONE_SELECTED
+const totalPrice = toys + shoes + batteries + pens + shirts  
+const totalCost = totalPrice + shipping 
 
-if (shoes + batteries + pens + shirts >= 1000 && currency === 'R' ) {  //added the value of 1000 and the currency
-    
-        if (location == 'NAM' || location == 'RSA' && customers !== 1) {
-            shipping = 0  
-	}
 
-        else {
+if (totalPrice >= 1000 && (location === 'RSA' || location === 'NAM')) {  //added the value of 1000 
+        if(customers === 1) {
+            shipping = 0
+        } else {
             console.log(FREE_WARNING) 
-
-    }
-    
+        }         
 }
 
-if(location === 'NK'){  //declared location as 'NK'
-    shipping = null
-    currency = null  //declared shipping as 'null'
-    console.log(BANNED_WARNING)
-}
 
-    
-
-console.log('price', currency, shoes + batteries + pens + shirts)
-
-
+console.log('price', currency,totalCost) 
