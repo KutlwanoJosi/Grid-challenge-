b802aa2ce46491474a08d41e2b85cebadf813ede
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-04-06 16:22:14 +0200

    Debugged and Completed Challenge 1 for IWA 8

--- IWA7/Challenge 3/scripts.js
+++ IWA7/Challenge 3/scripts.js
@@ -11,8 +11,8 @@ const divider = '----------------------------------'
 // Only change below this line
 
 const owed = parseFloat(`${(-1*leoBalance) + (-1*sarahBalance)}`)
-const leo = (`${leoName} ${leoSurname.trim()} Owed R: ${(-1*leoBalance).toFixed(2)}`)
-const sarah = (`${sarahName.trim()} ${sarahSurname} Owed R: ${(-1*sarahBalance).toFixed(2)}`)
+const leo =   (` ${leoName} ${leoSurname.trim()} (Owed R: ${(-1*leoBalance).toFixed(2)})`)
+const sarah = (`${sarahName.trim()} ${sarahSurname} (Owed R: ${(-1*sarahBalance).toFixed(2)})`)
 const total = (`Total amount owed: R${(owed).toFixed(2)}`)
 const result = (`${leo} \n ${sarah} \n \n ${divider} \n ${(total)} \n ${divider}`)
 
