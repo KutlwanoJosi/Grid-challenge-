d28d873ed2a3ba8fd902a290133f594a9fdb6d0c
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-04-12 18:50:31 +0200

    Debugging

--- IWA10/Challenge 1/scripts.js
+++ IWA10/Challenge 1/scripts.js
@@ -1,103 +1,87 @@
-const currentYear = new Date().getFullYear()
+const currentYear = new Date().getFullYear();
 
 const holidays = {
-    0: {
-        id: 0,
-        name: 'Day of Reconciliation',
-        date: `16 December ${currentYear}`,
-    },
-    1: {
-        id: 1,
-        name: 'Workers Day',
-        date: new Date(`1 April ${currentYear}`),
-    },
-    2: {
-        id: 2,
-        name: 'Day of Goodwill',
-        date: new Date(`26 December ${currentYear}`),
-    },
-    3: {
-        id: 3,
-        name: 'New Year Day',
-        date: new Date(`1 January ${currentYear}`),
-    },
-    4: {
-        id: 4,
-        name: 'Womens Day',
-        date: new Date(`9 August ${currentYear}`),
-    },
-    5: {
-        id: 5,
-        name: 'Heritage Day',
-        date: new Date(`24 September ${currentYear}`),
-    },
-    6: {
-        id: 6,
-        name: 'Christmas Day',
-        date: new Date(`25 December ${currentYear} 13:25`),
-    },
-    7: {
-        id: 7,
-        name: 'Youth Day',
-        date: new Date(`16 June ${currentYear}`),
-    },
-    8: {
-        id: 8,
-        name: 'Human Rights Day',
-        date: new Date(`21 March ${currentYear}`)
-    },
+  0: {
+    id: 0,
+    name: 'Day of Reconciliation',
+    date: `16 December ${currentYear}`,
+  },
+  1: {
+    id: 1,
+    name: 'Workers Day',
+    date: new Date(`1 April ${currentYear}`),
+  },
+  2: {
+    id: 2,
+    name: 'Day of Goodwill',
+    date: new Date(`26 December ${currentYear}`),
+  },
+  3: {
+    id: 3,
+    name: 'New Year Day',
+    date: new Date(`1 January ${currentYear}`),
+  },
+  4: {
+    id: 4,
+    name: 'Womens Day',
+    date: new Date(`9 August ${currentYear}`),
+  },
+  5: {
+    id: 5,
+    name: 'Heritage Day',
+    date: new Date(`24 September ${currentYear}`),
+  },
+  6: {
+    id: 6,
+    name: 'Christmas Day',
+    date: new Date(`25 December ${currentYear} 13:25`),
+  },
+  7: {
+    id: 7,
+    name: 'Youth Day',
+    date: new Date(`16 June ${currentYear}`),
+  },
+  8: {
+    id: 8,
+    name: 'Human Rights Day',
+    date: new Date(`21 March ${currentYear}`),
+  },
+};
+
+const christmas = 6;
+const futureId = 9;
+
+// check if item has been assigned to 9 key
+console.log(holidays[futureId] ? holidays[futureId].name : `ID ${futureId} not created yet`);
+
+// create a copy of Christmas object and modify name and date
+let copied = { ...holidays[christmas] };
+copied.name = 'X-mas Day';
+copied.date.setHours(0, 0, 0, 0);
+
+// check if new date is earlier
+const isEarlier = copied.date.getTime() < holidays[christmas].date.getTime();
+console.log('New date is earlier:', isEarlier);
+
+// if new date is earlier, update copied object
+if (isEarlier) {
+  const { date, ...rest } = copied;
+  copied = { ...rest, date: copied.date.toISOString().substr(0, 10) };
 }
 
-const christmas = 6
-const futureId = 9
+// log the changes made to copied object
+console.log(`ID change: false`);
+console.log(`Name change: ${copied.name}`);
+console.log(`Date change: ${copied.date}`);
 
-// Do not change code above this comment
+// output first and last holiday of the year in DD/MM/YYYY format
+const holidayDates = Object.values(holidays).map((holiday) => (holiday).date.getTime());
+const firstHoliday = new Date(Math.min(...holidayDates));
+const lastHoliday = new Date(Math.max(...holidayDates));
+console.log(`First holiday of the year: ${firstHoliday.getDate().toString().padStart(2, '0')}/${(firstHoliday.getMonth() + 1).toString().padStart(2, '0')}/${firstHoliday.getFullYear()}`);
+console.log(`Last holiday of the year: ${lastHoliday.getDate().toString().padStart(2, '0')}/${(lastHoliday.getMonth() + 1).toString().padStart(2, '0')}/${lastHoliday.getFullYear()}`);
 
-console.log(holidays.futureId.name || 'ID {futureId} not created yet')
-
-copied = holidays.christmas
-copied = { name: 'X-mas Day' }
-correctDate = copied.date
-correctDate.hours = 0
-correctDate.minutes = 0
-isEarlier = copied.date < holidays[6].date
-console.log('New date is earlier:', isEarlier)
-if (isEarlier) copied.date = correctDate
-console.log('ID change:', holidays[christmas].id != copied.id || copied.id)
-console.log('Name change:', holidays[christmas].name != copied.name || copied.name)
-console.log('Date change:', holidays[christmas].date != copied.date || copied.date)
-
-const firstHolidayTimestamp = Math.min(
-    holidays[0].date.getTime,
-    holidays[1].date.getTime,
-    holidays[2].date.getTime,
-    holidays[3].date.getTime,
-    holidays[4].date.getTime,
-    holidays[5].date.getTime,
-    holidays[6].date.getTime,
-    holidays[7].date.getTime,
-    holidays[8].date.getTime,
-)
-
-const lastHolidayTimestamp = Math.max(
-    holidays[0].date.getTime,
-    holidays[1].date.getTime,
-    holidays[2].date.getTime,
-    holidays[3].date.getTime,
-    holidays[4].date.getTime,
-    holidays[5].date.getTime,
-    holidays[6].date.getTime,
-    holidays[7].date.getTime,
-    holidays[8].date.getTime,
-)
-
-const firstDay = firstHolidayTimestamp.getDate
-const firstMonth = firstHolidayTimestamp.getMonth
-const lastDay = lastHolidayTimestamp.getDate
-const lastMonth = lastHolidayTimestamp.getMonth
-
-console.log('{firstDay}/{firstMonth}/{currentYear}')
-console.log('{lastDay}/{lastMonth}/{currentYear}')
-
-const randomHoliday = holidays[Math.random]
-console.log(randomHoliday.date)
+// output a random holiday date in DD/MM/YYYY format
+const randomHolidayIndex = Math.floor(Math.random() * Object.keys(holidays).length);
+const randomHoliday = Object.values(holidays)[randomHolidayIndex];
+console.log(`Random holiday`)
