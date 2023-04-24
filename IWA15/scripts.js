a62cfee1b134da54d53a0805de3682c28d10d450
Kutlwano Josi <kutlwanojosi01@gmail.com>
2023-04-24 16:19:28 +0200

    Created IWA 16 Files

--- IWA15/scripts.js
+++ IWA15/scripts.js
@@ -6,20 +6,30 @@ const data = {
       ['third', [9, 3, 1]],
     ]
   }
-  // Only edit below
-  const first = data.lists[0][1];
-  const second = data.lists[1][1];
-  const third = data.lists[2][1];
-  const result = [];
-     const extractBiggest = () => {
-     if (first[first.length - 1] >= second[second.length - 1] && first[first.length -1] >= third[third.length - 1]) {
-       return first.pop();
+
+  // Initialising an object "data" with a single property "lists" which is an array containing three sub-arrays.
+  const first = data.lists[0][1];   //Fixed errors and used the square brackets 
+  const second = data.lists[1][1];  //Fixed errors and used the square brackets
+  const third = data.lists[2][1];   //Fixed errors and used the square brackets
+
+  const result = []; //this is an empty array
+
+  /*This function contains three `if` statements to determine which array contains the largest value.
+  It compares the last element of each array (`first`, `second`, and `third`) to determine which one is the largest.*/
+
+  const extractBiggest = () => {
+    //If `first` has the largest value, the function removes the last element from the `first` array and returns it.
+    if (first[first.length - 1] >= second[second.length - 1] && first[first.length -1] >= third[third.length - 1]) {
+      return first.pop();
       }
-     if (second[second.length - 1] >= third[third.length - 1]) {
-        return second.pop();
+      //If `second` has the largest value, it removes the last element from `second` array and returns it.
+    if (second[second.length - 1] >= third[third.length - 1]) {
+      return second.pop();
       }
+      // If `third` has the largest value,it removes the last element from `third` array and returns it.
       return third.pop();
   }
+
   // Only edit above
   result.push(extractBiggest())
   result.push(extractBiggest())
